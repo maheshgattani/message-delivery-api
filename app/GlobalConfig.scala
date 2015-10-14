04f0f35508d01f0ef45c13facd5ff2f7b5aaa9f8
Mahesh Gattani <maheshgattani@gmail.com>
2015-10-14 09:19:34 -0400

    better form validation and configurable number of producers

--- app/GlobalConfig.scala
+++ app/GlobalConfig.scala
@@ -1,10 +1,13 @@
 package app
 import akka.actor.{Props, ActorSystem}
 import akka.routing.RoundRobinPool
+import play.Play
 import producers.MessageProducer
 
 object GlobalConfig {
+	val config = Play.application.configuration.getConfig("rabbitmq")
 	val messageProducerSystem = ActorSystem("messageProducerSystem")
+	val numberOfProducers = config.getString("number-of-producers").toInt
 	val messageProducerRouter = messageProducerSystem.actorOf(
-		Props[MessageProducer].withRouter(RoundRobinPool(5)), name = "messageProducerRouter")
+		Props[MessageProducer].withRouter(RoundRobinPool(numberOfProducers)), name = "messageProducerRouter")
 }
--- app/controllers/MessageProducerController.scala
+++ app/controllers/MessageProducerController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import akka.util.Timeout
 import app.GlobalConfig
+import play.api.libs.json.Json
 import play.api.mvc._
 import producers.Message
 import akka.pattern.ask
@@ -9,25 +10,33 @@ import scala.concurrent.duration._
 import scala.concurrent.Await
 import play.api.data._
 import play.api.data.Forms._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 
 class MessageProducerController extends Controller {
 
 	implicit val timeout = Timeout(1 minutes)
 	val postForm = Form(
 		mapping(
-			"data" -> text,
-			"routing_key" -> text,
-			"exchange" -> text
+			"data" -> nonEmptyText,
+			"routing_key" -> nonEmptyText,
+			"exchange" -> nonEmptyText
 		)(Message.apply)(Message.unapply)
 	)
 
 	def post = Action { implicit request =>
-		val message = postForm.bindFromRequest().get
-		val future = GlobalConfig.messageProducerRouter ? message
-		val didPublish: Boolean = Await.result(future.mapTo[Boolean], 1 minute)
-		if (didPublish)
-			Ok
-		else
-			ServiceUnavailable
+		postForm.bindFromRequest().fold (
+			formWithErrors => {
+				BadRequest(Json.stringify(formWithErrors.errorsAsJson))
+			},
+			message => {
+				val future = GlobalConfig.messageProducerRouter ? message
+				val didPublish: Boolean = Await.result(future.mapTo[Boolean], 1 minute)
+				if (didPublish)
+					Ok
+				else
+					ServiceUnavailable
+			}
+		)
 	}
 }
